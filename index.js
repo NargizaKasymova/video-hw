c7e4753627cac7f9b8b02933ec9254e3d13e73c3
Nargiza <n.kasymova2021@gmail.com>
2021-06-22 18:06:17 +0600

    query added

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ app.use(express.json())
 const PORT = 5050
 
 app.get('/', (req, res) => {
-    res.status(201).json('Server works')
+    console.log(req.query)
+    console.log(req.query.test)
+    
+    res.status(201).json('Server works123')
 })
+
+
 
 app.listen(PORT, function() {
     console.log('App has been satrted ' + PORT)
